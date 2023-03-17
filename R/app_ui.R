6b96bc1d265ec31b7430edccbc6f838eeea524c2
Justin Townsend <justinjtownsend@gmail.com>
2023-03-17 16:29:06 +0000

    Adjustments to get a simple table working in Shiny with golem. More work to do. Might just get the functions for retrieving the data working well first. ;-)

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,7 +14,6 @@ app_ui <- function(request) {
     )
   )
 }
-
 #' Add external Resources to the Application
 #'
 #' This function is internally used to add external
---
+++ R/mod_getDatafromSource.R
@@ -0,0 +1,31 @@
+#' getDatafromSource UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_getDatafromSource_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' getDatafromSource Server Functions
+#'
+#' @noRd 
+mod_getDatafromSource_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+ 
+  })
+}
+    
+## To be copied in the UI
+# mod_getDatafromSource_ui("getDatafromSource_1")
+    
+## To be copied in the server
+# mod_getDatafromSource_server("getDatafromSource_1")
